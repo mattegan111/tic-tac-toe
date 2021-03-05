9b5cfd75d6cfd6dfd48c1eb8eb137585cc485e9c
mattegan111 <mnegan93@gmail.com>
2021-03-05 22:19:41 +1300

    Added check for winner or draw

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const Player = (name, marker) => {
+    
     const getName = () => name;
     const getMarker = () => marker;
 
     return {getName, getMarker};
-}
+};
 
 const gameBoard = (() => {
     
     var marks = 
-     ["a1", "a2", "a3",
-      "b1", "b2", "b3",
-      "c1", "c2", "c3"];
+     ["0", "1", "2",
+      "3", "4", "5",
+      "6", "7", "8"];
     
     const getMarks = () => {
 		return marks;
     };
+
+    const updateMarks = (id, turn) => {
+        marks[id] = turn;
+    };
     
-    return getMarks;
+    return {getMarks, updateMarks};
 });
 
 const controller = (() => {
 
-    const checkWinner = () => {
-        const board = gameBoard.getMarks();
+    const checkWinner = (turn) => {
+
+        var gameOver = false;
+        
+        const boardMarks = boardInstance.getMarks();
 
         const winConditions = [
             [0, 1, 2],
@@ -34,6 +42,42 @@ const controller = (() => {
             [0, 4, 8],
             [2, 4, 6],
         ];
+
+        for (let i = 0; i < winConditions.length; i++) {
+
+            for (let j = 0; j < 3; j++) {
+                
+                if (boardMarks[winConditions[i][j]] != turn) {
+                    break;
+                }
+                
+                else if (j == 2 && boardMarks[winConditions[i][j]] == turn) {
+                    alert(turn + " is the winner");
+                    gameOver = true;
+                }
+            };
+        };
+
+        // check for draw
+
+        if (gameOver == false) {
+            
+            var draw = true;
+
+            for (let k = 0; k < boardMarks.length; k++) {
+    
+                if (boardMarks[k] != "x" && boardMarks[k] != "o" ) {
+                    
+                    draw = false;
+                }
+            };
+    
+            if (draw == true) {
+                
+                alert("It's a tie!");
+            }
+        }
+
     };
 
     const init = () => {
@@ -63,12 +107,24 @@ const controller = (() => {
                 if (turn == "x"){
                     e.target.innerHTML = "<h3>x</h3>";
                     e.target.classList = "grid-item";
+
+                    const id = e.target.id;
+                    boardInstance.updateMarks(id, turn);
+
+                    controllerInstance.checkWinner(turn);
+
                     turn = "o";
                 }
         
                 else if (turn == "o"){
                     e.target.innerHTML = "<h4>o</h4>";
                     e.target.classList = "grid-item";
+
+                    const id = e.target.id;
+                    boardInstance.updateMarks(id, turn);
+
+                    controllerInstance.checkWinner(turn);
+
                     turn = "x";
                 }
             }
@@ -111,6 +167,7 @@ const controllerInstance = controller();
 controllerInstance.init();
 const domInstance = DOM();
 domInstance.renderPlayers();
+const boardInstance = gameBoard();
 
 
 var turn = "x";
